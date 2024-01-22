990e2edb820b9c2be2326fce55f058549a1f6989
Laura Borowski <laura.borowski@innogames.com>
2024-01-22 14:05:27 +0100

    make work

---
+++ src/main/java/org/school/freshanddrippy/FreshAndDrippyApplication.java
@@ -0,0 +1,13 @@
+package org.school.freshanddrippy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshAndDrippyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshAndDrippyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/school/freshanddrippy/FreshAndDrippyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.school.freshanddrippy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshAndDrippyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
