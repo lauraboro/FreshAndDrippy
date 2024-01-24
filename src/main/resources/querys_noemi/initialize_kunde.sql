95ae2c5a69752873555b608cb2e57e66d443d411
noemi.klimpel <noemi.klimpel@innogames.com>
2024-01-24 13:21:09 +0100

    Insert data for customers

---
+++ src/main/resources/querys_noemi/initialize_kunde.sql
@@ -0,0 +1,11 @@
+INSERT INTO KUNDE (name, vorname, geburtstag, telefonnummer, email)
+VALUES
+    ('Wellensteyn', 'Kira', '1990-05-05', '040/443322', 'k.wellensteyn@yahoo.de'),
+    ('Foede', 'Dorothea', '2000-03-24', '040/543822', 'd.foede@web.de'),
+    ('Leberer', 'Sigrid', '1989-09-21', '0175/1234588', 'sigrid@leberer.de'),
+    ('Soerensen', 'Hanna', '1974-04-03', '040/634578', 'h.soerensen@yahoo.de'),
+    ('Schnitter', 'Marten', '1964-04-17', '0176/447587', 'schni_mart@gmail.com'),
+    ('Maurer', 'Belinda', '1978-09-09', '040/332189', 'belinda1978@yahoo.de'),
+    ('Gessert', 'Armin', '1978-01-29', '040/67890', 'armin@gessert.de'),
+    ('Haessig', 'Jean-Marc', '1982-08-30', '0178-67013390', 'jm@haessig.de'),
+    ('Urocki', 'Eric', '1999-12-04', '0152-96701390', 'urocki@outlook.de');
